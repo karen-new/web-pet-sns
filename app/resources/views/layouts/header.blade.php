00b25db9d9133e353a071ef49ba65c870d74b3b2
karen-new <k.renren0119@gmail.com>
2023-10-27 12:40:51 +0900

    ヘッダーの設定リンク間違い修正 #2

--- app/resources/views/layouts/header.blade.php
+++ app/resources/views/layouts/header.blade.php
@@ -18,7 +18,7 @@
         <a href="{{ route('search') }}" class="btn my-3 btn-green" style="font-size: 20px;">
             <i class="fa fa-search pe-1" aria-hidden="true" style="color: white;"></i>検索
         </a>
-        <a href="{{ route('search') }}" class="btn my-3 btn-green" style="font-size: 20px;">
+        <a href="{{ route('profile.personal') }}" class="btn my-3 btn-green" style="font-size: 20px;">
             <i class="fas fa-cog pe-1" style="color: white;"></i>設定
         </a>
         @if (Route::has('login'))
